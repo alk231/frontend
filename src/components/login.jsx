6cc3e4e8edd6d31476ee2573fb026463b225f799
ak9099 <ak9931927833@gmail.com>
2024-06-27 19:20:57 +0530

    first commit

---
+++ src/components/login.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { loginFields } from "./formField";
+import FormAction from "./formAction";
+import FormExtra from "./formExtra";
+import Input from "./input";
+
+const fields=loginFields;
+let fieldsState = {};
+fields.forEach(field=>fieldsState[field.id]='');
+
+export default function Login(){
+    const [loginState,setLoginState]=useState(fieldsState);
+
+    const handleChange=(e)=>{
+        setLoginState({...loginState,[e.target.id]:e.target.value})
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        authenticateUser();
+    }
+
+    //Handle Login API Integration here
+    const authenticateUser = () =>{
+            console.log("Authentic user",loginState)
+    }
+
+    return(
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+        <div className="-space-y-px">
+            {
+                fields.map(field=>
+                        <Input
+                            key={field.id}
+                            handleChange={handleChange}
+                            value={field.value}
+                            labelText={field.labelText}
+                            labelFor={field.labelFor}
+                            id={field.id}
+                            name={field.name}
+                            type={field.type}
+                            isRequired={field.isRequired}
+                            placeholder={field.placeholder}
+                    />
+                
+                )
+            }
+        </div>
+
+        <FormExtra/>
+        <FormAction handleSubmit={handleSubmit} text="Login"/>
+
+      </form>
+    )
+}
